678a05d7d15723ac8680a982511eb51d0639a875
XyrWL <peter.trullenque@gmail.com>
2015-12-22 14:06:10 +0100

    Exercise 2 draft done.

---
+++ Exercise2/Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Exercise2.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="ButtonLoadCategories" runat="server" OnClick="ButtonLoadCategories_OnClick" Text="Load Product Categories"/>
+            <asp:DropDownList ID="DropDownCategories" runat="server" AutoPostBack="True"/>
+            <asp:Label ID="LabelException" runat="server" Text="" Visible="False"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Exercise2/Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Exercise2
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void ButtonLoadCategories_OnClick(object sender, EventArgs e)
+        {
+            SqlConnection sqlConnection = new SqlConnection("Server=localhost; Database=AdventureWorks2012; Integrated Security=true");
+
+            string sqlQuery = "SELECT * FROM Production.ProductCategory";
+            SqlCommand sqlCommand = new SqlCommand(sqlQuery, sqlConnection);
+            SqlDataReader sqlDataReader = null;
+
+            DropDownCategories.Items.Clear();
+            ListItem defaultItem = new ListItem { Text = "Select", Value = "0" };
+            DropDownCategories.Items.Add(defaultItem);
+            try
+            {
+                sqlConnection.Open();
+                sqlDataReader = sqlCommand.ExecuteReader();
+                while (sqlDataReader.Read())
+                {
+                    ListItem item = new ListItem();
+                    item.Text = sqlDataReader["Name"].ToString();
+                    item.Value = sqlDataReader["ProductCategoryID"].ToString();
+                    DropDownCategories.Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                LabelException.Text = ex.Message;
+                LabelException.Visible = true;
+            }
+            finally
+            {
+                if (sqlDataReader != null)
+                {
+                    sqlDataReader.Close();
+                    sqlDataReader.Dispose();
+                }
+                sqlConnection.Close();
+                sqlConnection.Dispose();
+                sqlCommand.Dispose();
+            }
+        }
+    }
+}
